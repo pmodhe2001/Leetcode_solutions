b85243fa72f78dfcc6779f03ff281a70f957ced7
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2021-08-13 10:40:47 +0530

    Create Pair , Set and Unordered Map.cpp

---
+++ Multiple Data Structures/Pair , Set and Unordered Map.cpp
@@ -0,0 +1,23 @@
+//Problem link :  https://leetcode.com/problems/k-diff-pairs-in-an-array/
+//Time Complexity : 
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        
+         set<pair<int,int>> pr; // set of pairs , because we want pairs to be unique
+         unordered_map<int,bool> mp;  // key of map is vector element , value is true(present in vector)
+        // we made unordered map because it takes O(1) time to search in it 
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<nums.size();i++){
+            int temp=nums[i]-k;  // find temp
+            if(mp.find(temp)!=mp.end()){ // search temp in map
+                pr.insert(make_pair(nums[i],temp));  // if found insert pairs in set of pairs
+            }
+            mp[nums[i]]=true;  // mark true in map that nums[i] is present in vector
+        }
+        return pr.size(); // return size of set
+    }
+        
+};
