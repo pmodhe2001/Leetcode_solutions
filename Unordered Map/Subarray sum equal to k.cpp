7eb0b5a95bfcc4e6f49513b53a1ca3fd9c643b79
Prathmesh Modhe <49117568+pmodhe2001@users.noreply.github.com>
2021-09-21 23:17:32 +0530

    Create Subarray sum equal to k.cpp

---
+++ Unordered Map/Subarray sum equal to k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int sum=0;
+        int res=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            res+=mp[sum-k];
+            mp[sum]++;
+        }
+      return res;
+   }
+};
